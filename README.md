062f8156f01f51cfd64a95984dc043af26f2119a
Hugo Winder <hugo@mdlonline.nl>
2022-11-29 14:46:58 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,38 @@
+# Revalidate NextJS URL
+
+This plugin is used to revalidate the NextJS URL when saving a post or page. It is used in conjunction with the NextJS revalidate api.
+
+This plugin is intended for the use with headless WordPress and NextJS. It assumes you have setup on demand revalidation in NextJS.
+
+This can be done by adding the following file to your NextJS project:
+
+```js
+// pages/api/revalidate.js
+export default async function handler(req, res) {
+    const { path, token } = req.query;
+
+    if (token !== process.env.SECRET_TOKEN) {
+        return res.status(401).json({ message: "Invalid token" });
+    } else if (path.length === 0) {
+        return res.status(401).json({ message: "Path is required" });
+    }
+
+    try {
+        await res.revalidate(path);
+    } catch (err) {
+        return res.status(500).send("Error revalidating page");
+    }
+
+    return res.status(200).json({
+        revalidated: true,
+        message: `Path ${path} revalidated successfully`,
+    });
+}
+```
+
+## Installation
+Download the plugin as a zip file and upload it to your WordPress site. Activate the plugin.
+This will add a new settings page to your WordPress site. You will need to add a secret token to the settings page. This token will be used to authenticate the revalidation request. Also, you will need to add the URL to your NextJS project like this `https://example.com/api/revalidate` or `http://localhost:3000/api/revalidate`
+
+## Usage
+Once the plugin is installed and activated with the correct setting, this plugin will automatically revalidate the NextJS URL when a post or page is saved.
---
+++ ravalidate-url.php
@@ -0,0 +1,136 @@
+<?php
+/*
+Plugin Name:  Revalidate NextJS URL
+Description:  A small plugin that will revalidate the URL of a post when it is saved. Works with NextJS revalidate.
+Version:      0.1
+Author:       Hugo Winder
+Author URI:   https://www.hugowinder.com
+Plugin URI:   https://github.com/Hugow1/WP-Revalidate-NextJS-URL
+License:      GPL3
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  wp-revalidate-nextjs-url
+Domain Path:  /languages
+*/
+
+add_action('save_post', 'revalidate_post', 20, 1);
+
+$wp_revalidate_nextjs_url_options = get_option( 'wp_revalidate_nextjs_url_option_name' );
+define('SECRET_TOKEN', $wp_revalidate_nextjs_url_options['secret_token_0']);
+define('NEXTJS_URL', $wp_revalidate_nextjs_url_options['nextjs_revalidate_api_path_1']);
+
+function revalidate_post($post_id) {
+  $post = get_post($post_id);
+  $url = get_permalink($post_id);
+  $url = str_replace(site_url(), '', $url);
+
+  $curl = curl_init(NEXTJS_URL . "?path=" . $url . "&token=" . SECRET_TOKEN);
+
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+
+  $response = curl_exec($curl);
+  curl_close($curl);
+}
+
+// Settings page in admin
+class WPRevalidateNextjsURL {
+	private $wp_revalidate_nextjs_url_options;
+
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'wp_revalidate_nextjs_url_add_plugin_page' ) );
+		add_action( 'admin_init', array( $this, 'wp_revalidate_nextjs_url_page_init' ) );
+	}
+
+	public function wp_revalidate_nextjs_url_add_plugin_page() {
+		add_options_page(
+			'WP Revalidate Nextjs URL', // page_title
+			'WP Revalidate Nextjs URL', // menu_title
+			'manage_options', // capability
+			'wp-revalidate-nextjs-url', // menu_slug
+			array( $this, 'wp_revalidate_nextjs_url_create_admin_page' ) // function
+		);
+	}
+
+	public function wp_revalidate_nextjs_url_create_admin_page() {
+		$this->wp_revalidate_nextjs_url_options = get_option( 'wp_revalidate_nextjs_url_option_name' ); ?>
+
+		<div class="wrap">
+			<h2>WP Revalidate Nextjs URL</h2>
+			<p>Add your secret token and the path to the api endpoint here.</p>
+			<?php settings_errors(); ?>
+
+			<form method="post" action="options.php">
+				<?php
+					settings_fields( 'wp_revalidate_nextjs_url_option_group' );
+					do_settings_sections( 'wp-revalidate-nextjs-url-admin' );
+					submit_button();
+				?>
+			</form>
+		</div>
+	<?php }
+
+	public function wp_revalidate_nextjs_url_page_init() {
+		register_setting(
+			'wp_revalidate_nextjs_url_option_group', // option_group
+			'wp_revalidate_nextjs_url_option_name', // option_name
+			array( $this, 'wp_revalidate_nextjs_url_sanitize' ) // sanitize_callback
+		);
+
+		add_settings_section(
+			'wp_revalidate_nextjs_url_setting_section', // id
+			'Settings', // title
+			array( $this, 'wp_revalidate_nextjs_url_section_info' ), // callback
+			'wp-revalidate-nextjs-url-admin' // page
+		);
+
+		add_settings_field(
+			'secret_token_0', // id
+			'Secret token', // title
+			array( $this, 'secret_token_0_callback' ), // callback
+			'wp-revalidate-nextjs-url-admin', // page
+			'wp_revalidate_nextjs_url_setting_section' // section
+		);
+
+		add_settings_field(
+			'nextjs_revalidate_api_path_1', // id
+			'Nextjs revalidate api path', // title
+			array( $this, 'nextjs_revalidate_api_path_1_callback' ), // callback
+			'wp-revalidate-nextjs-url-admin', // page
+			'wp_revalidate_nextjs_url_setting_section' // section
+		);
+	}
+
+	public function wp_revalidate_nextjs_url_sanitize($input) {
+		$sanitary_values = array();
+		if ( isset( $input['secret_token_0'] ) ) {
+			$sanitary_values['secret_token_0'] = sanitize_text_field( $input['secret_token_0'] );
+		}
+
+		if ( isset( $input['nextjs_revalidate_api_path_1'] ) ) {
+			$sanitary_values['nextjs_revalidate_api_path_1'] = sanitize_text_field( $input['nextjs_revalidate_api_path_1'] );
+		}
+
+		return $sanitary_values;
+	}
+
+	public function wp_revalidate_nextjs_url_section_info() {
+
+	}
+
+	public function secret_token_0_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_revalidate_nextjs_url_option_name[secret_token_0]" id="secret_token_0" value="%s">',
+			isset( $this->wp_revalidate_nextjs_url_options['secret_token_0'] ) ? esc_attr( $this->wp_revalidate_nextjs_url_options['secret_token_0']) : ''
+		);
+	}
+
+	public function nextjs_revalidate_api_path_1_callback() {
+		printf(
+			'<input class="regular-text" type="text" name="wp_revalidate_nextjs_url_option_name[nextjs_revalidate_api_path_1]" id="nextjs_revalidate_api_path_1" value="%s">',
+			isset( $this->wp_revalidate_nextjs_url_options['nextjs_revalidate_api_path_1'] ) ? esc_attr( $this->wp_revalidate_nextjs_url_options['nextjs_revalidate_api_path_1']) : ''
+		);
+	}
+
+}
+if ( is_admin() )
+	$wp_revalidate_nextjs_url = new WPRevalidateNextjsURL();
